cf4c1f6dd1bb965a2165e5874ea0da3d17654419
SushkaCraft <102476271+SushkaCraft@users.noreply.github.com>
2024-10-03 15:07:54 +0300

    Fix Task1.py

--- Task1.py
+++ Task1.py
@@ -2,137 +2,132 @@
 
 name, age, location = input("Ваши Фамилия, Имя?:\n"), input("Сколько Вам лет?:\n"), input("Где Вы живете?:\n")
 
-print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Ваш возраст: {age}\n
-      Вы живете в: {location}\n
-""")
+print(f"""Ваши Фамилия, Имя: {name}
+Ваш возраст: {age}
+Вы живете в: {location}\n""")
 
 # Variant 2
 
 name, age, education = input("Ваше Имя?:\n"), input("Ваша Дата Рождения?:\n"), input("Где Вы Учитсь?:\n")
 
-print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Дата рождения: {age}\n
-      Вы учитесь в: {education}\n
-""")
+print(f"""Ваши Фамилия, Имя: {name}
+Дата рождения: {age}
+Вы учитесь в: {education}\n""")
 
 # Variant 3
 
 name, location = input("Ваши Фамилия?:\n"), input("Где Вы живете?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Вы живете в: {location}\n
-""")
+Ваши Фамилия, Имя: {name}
+Вы живете в: {location}
+\n""")
 
 # Variant 4
 
 name, location, music = input("Ваши Фамилия?:\n"), input("Где Вы Родились?:\n"), input("Какая музыка нравится?:\n")
 
-print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Вы живете в: {location}\n
-      Ваша любимая музыка: {music}\n
-""")
+print(f"""Ваши Фамилия, Имя: {name}
+Вы живете в: {location}
+Ваша любимая музыка: {music}
+\n""")
 
 # Variant 5
 
 name, age, location = input("Ваши Фамилия, Имя, Отчество?:\n"), input("Сколько Вам лет?:\n"), input("Где Вы живете?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя, Отчество: {name}\n
-      Ваш возраст: {age}\n
-      Вы живете в: {location}\n
-""")
+Ваши Фамилия, Имя, Отчество: {name}
+Ваш возраст: {age}
+Вы живете в: {location}
+\n""")
 
 # Variant 6
 
 name, love_subject, class_number = input("Ваше Имя?:\n"), input("Какой ваш любимый предмет в школе?:\n"), input("В каком классе вы учитесь?:\n")
 
 print(f"""
-      Ваши Имя: {name}\n
-      Ваш любимый предмет: {love_subject}\n
-      Вы учитесь в классе номер: {class_number}\n
-""")
+Ваши Имя: {name}
+Ваш любимый предмет: {love_subject}
+Вы учитесь в классе номер: {class_number}
+\n""")
 
 # Variant 7
 
 name, hobby = input("Ваши Фамилия, Имя, Отчество?:\n"), input("Чем Вы Увлекаетесь?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя, Отчество: {name}\n
-      Ваше хобби: {hobby}\n
-""")
+Ваши Фамилия, Имя, Отчество: {name}
+Ваше хобби: {hobby}
+\n""")
 
 # Variant 8
 
 name, education, teacher_name = input("Ваше Имя?:\n"), input("Где Вы Учитсь?:\n"), input("ФИО вашего преподователя по информатике\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Вы учились в школе: {education}\n
-      ФИО вашего преподователя по информатик: {teacher_name}\n
-""")
+Ваши Фамилия, Имя: {name}
+Вы учились в школе: {education}
+ФИО вашего преподователя по информатик: {teacher_name}
+\n""")
 
 # Variant 9
 
 name, love_subject, curator_name = input("Ваше Имя?:\n"), input("Какой ваш любимый предмет в школе?:\n"), input("ФИО вашего куратора?:\n")
 
 print(f"""
-      Ваши Имя: {name}\n
-      Ваш любимый предмет: {love_subject}\n
-      ФИО вашего куратора: {curator_name}\n
-""")
+Ваши Имя: {name}
+Ваш любимый предмет: {love_subject}
+ФИО вашего куратора: {curator_name}
+\n""")
 
 # Variant 10
 
 name, age, date = input("Ваши Фамилия, Имя?:\n"), input("Сколько Вам лет?:\n"), input("Когда вы родились?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Ваш возраст: {age}\n
-      Вы родились: {location}\n
-""")
+Ваши Фамилия, Имя: {name}
+Ваш возраст: {age}
+Вы родились: {location}
+\n""")
 
 # Variant 11
 
 name, location, birth_location = input("Ваши Фамилия, Имя?:\n"), input("Где Вы живете?:\n"), input("Где Вы Родились?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Вы живете в: {location}\n
-      Вы родились в: {birth_location}\n
-""")
+Ваши Фамилия, Имя: {name}
+Вы живете в: {location}
+Вы родились в: {birth_location}
+\n""")
 
 # Variant 12
 
 name, age, phone = input("Ваши Фамилия, Имя?:\n"), input("Сколько Вам лет?:\n"), input("Номер вашего телефона?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Ваш возраст: {age}\n
-      Ваш номер телефона: {phone}\n
-""")
+Ваши Фамилия, Имя: {name}
+Ваш возраст: {age}
+Ваш номер телефона: {phone}
+\n""")
 
 # Variant 13
 
 name, age, country, edge, city = input("Ваши Фамилия, Имя?:\n"), input("Сколько Вам лет?:\n"), input("В какой стране вы живете?:\n"), input("В каком краю вы живете?:\n"), input("В каком городе вы живете?:\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      Ваш возраст: {age}\n
-      Вы живете в стране: {country}\n
-      Вы живете в крае: {edge}\n
-      Вы живете в городе: {city}\n
-""")
+Ваши Фамилия, Имя: {name}
+Ваш возраст: {age}
+Вы живете в стране: {country}
+Вы живете в крае: {edge}
+Вы живете в городе: {city}
+\n""")
 
 # Variant 14
 
 name, teacher_name = input("Ваше Имя?:\n"), input("ФИО вашего классного руководителя\n")
 
 print(f"""
-      Ваши Фамилия, Имя: {name}\n
-      ФИО вашего классного руководителя: {teacher_name}\n
-""")
+Ваши Фамилия, Имя: {name}
+ФИО вашего классного руководителя: {teacher_name}
+\n""")
