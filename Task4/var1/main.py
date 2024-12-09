8f81f5e6bf584b42ef262843e970ae566d939ca0
SushkaCraft <102476271+SushkaCraft@users.noreply.github.com>
2024-12-09 09:00:23 +0000

    add v1 task4

--- Task4/var1/main.py
+++ Task4/var1/main.py
@@ -1,1 +1,16 @@
+import math
 
+x, y = int(input("x: ")), int(input("y: "))
+
+c = math.sqrt(math.log10(1/math.cos(x)) + y**2)
+
+k = (math.cos(x)**2 + abs(math.sin(c**2)))**1/3
+
+a = 3**(-c)*((math.sin(math.pi*k)/(math.pi*k)))
+
+t = c - a * math.exp(-2) + math.atan(k)
+
+print(c)
+print(k)
+print(a)
+print(t)
